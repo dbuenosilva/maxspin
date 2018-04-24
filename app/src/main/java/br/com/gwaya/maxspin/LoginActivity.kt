42d00ed9137a865e20c394acc49992c216ab900a
Diego Bueno <diego@bueno.eti.br>
2018-04-24 00:02:39 -0300

    Prévia com autenticação FireBase.

--- app/src/main/java/br/com/gwaya/maxspin/LoginActivity.kt
+++ app/src/main/java/br/com/gwaya/maxspin/LoginActivity.kt
@@ -2,11 +2,34 @@ package br.com.gwaya.maxspin
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+
 
 class LoginActivity : AppCompatActivity() {
+
+    private var mAuthListener: FirebaseAuth.AuthStateListener? = null
+    private var mAuth: FirebaseAuth? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+
+        mAuth = FirebaseAuth.getInstance()
+
+    }
+
+
+    override fun onStart() {
+        super.onStart()
+
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = mAuth?.getCurrentUser()
+        updateUI(currentUser)
+    }
+
+    fun updateUI(mAuth: FirebaseUser? ) {
+
     }
 }
